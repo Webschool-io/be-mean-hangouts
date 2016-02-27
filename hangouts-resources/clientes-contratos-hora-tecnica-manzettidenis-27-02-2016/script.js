77ec5c3c3e254b279aa2e099d1f203f05d8b2fca
Denis Manzetti <denis@swdigital.com.br>
2016-02-27 18:14:33 -0300

    material de apoio hangout clientes, hora tecina e contrato. por: manzettidenis

---
+++ hangouts-resources/clientes-contratos-hora-tecnica-manzettidenis-27-02-2016/script.js
@@ -0,0 +1,105 @@
+// Aqui são minahs anotações pessoais para ajudar a seguir o roteiro do workshop
+// e algumas anotações que julgo importante não esquecer.
+
+let roteiro[0] =  {
+    clientes = [
+         tipos de clientes,
+         brilhar os olhos,
+         escutar,
+         empatia,
+         apresentação,
+         {
+         burocratas = [
+             'os sócios',
+	       'a esposa',
+	       'o folgado',
+             'o desconfiado',
+             'o tal PJ',
+		  'CLT'
+         ]}
+   ]
+
+};
+
+let roteiro[1] = {
+
+    contrato = [
+        'quando fazer',
+        'modelos',
+        'tipos de licença',
+	      'como receber sempre',
+        'nao recebi, e agora',
+    ]
+}
+
+let roteiro[2] = {
+
+    horaTecnica = [
+        'como calcular',
+        'orçando com precisao',
+        'negociando sem perder',
+        'modelos de negócio que eu tentei'
+     ],
+
+}
+
+
+ ## Tipos de clientes:
+ - Pode ser cliente agencia, cliente final, cliente leigo, empresa, institucional, sistema..
+ cada tipo de produto e publico alvo tem que ter uma abordagem especial,.
+
+- Venda ativa - receptiva.
+
+- Como fazer uma proposta interessante.
+
+- Como vender mais barato sem sair perdendo.
+
+## Brilhar os olhos
+- Cliente quer ver layout, se você não é designer, faça parcerias, tenha modelos pré-prontos..
+não adianta voce falar tecnês, e você nao tem que fazer o cliente se sentir burro.. a ideia
+é conduzilo a entender o que voce está oferecendo, faça isso dando exemplos, por exemplo, responsivo.
+Em vez de apenas explicar o que um site responsivo, mostre pra ele um site NÃO responsivo.. use o simulador
+do chrome ou qualquer outro e faça ele sentir a experiencia horrível que é aquilo, depois, pelo celular, procure pelo dominio do site
+no google.. ele vai perceber SOZINHO a importancia daquilo, e vai agregar muito mais valor pra ele, do que voce expliando e fazendo a
+cabeça dele girar.
+
+- Pare de atirar para todo lado, especifique seu publico, se prepare para ele, estude o publico dele ! SEJA EXPERT no negocio dele, ai
+sim, faça uma lista de prospectos ( prospectos são pessoas fisica ou jurídica com grande potencial de se tornar seu cliente, seja por um problema em comun,
+ou fator financeiro, cargo na empresa).
+
+MEU EXEMPLO:
+Eu me especializei no setor da saude, antes queria mandar proposta pra 100 clientes por semana, mas agora fazendo isso para 3, e  ultimamente, nem mandando mais eu estou
+pois, por me especializar em um setor, chegar com soluções ja estruturadas, um layoutzinho pro cara PERSONALIZADO, nada de template... pra eu enviar uma proposta eu preciso
+ter 85% de certeza q o cara fecha, por que o layout que e envio na proposta, geralmente é o  definitivo, por que eu ja faço como se fosse para produção.
+Da trbalho? dá ?
+da resultado ? também dá.
+
+
+## Escutar
+    As vezes, por eu saber tanto do que eu faço, e querer FALAR da solução, a gente acaba deixando de prestar a atenção no problema.
+    recentemente eu prospectei um DR, e fechamos. Eu insistia em por conteudo, tornar o mais funcional possivel.. e deixei de notar
+    qeu ele nao queria mais consulta, nao queria falar de dores, ele queria CIRURGIA. isso foi um insight que eu tive, pois na ultima reunião,
+    eu mudei completamente minha postura e peguei detalhes valiosos que então fazendo com que o projeto fique MUITO mais melhor que eu pensava,
+    e o cliente MUITO mais satisfeito, então, as vezes.. escutar o cliente, vale mais e fecha a venda, do que querer mostrar que da pra fazer mais !
+
+    Faz o que ele quer. depois você implementa mais. se ele ficar satisfeito, ele vai te escutar.
+
+## Empatia
+- design thinking.
+
+## Apresentação
+ - Modelo de apresentação da SW Digital.
+ - Modelo da apresentação da SW Labs
+
+
+## Hora Tecnica
+- como calcular
+  - estudo aplicado
+  - eu manjo e faço rapido
+  - eu manjo e demoro
+
+- orçando com precisao,
+   SEMPRE DOBRE o que voce acha que vai demorar.
+- negociando sem perder
+ - ai entramos com a REBARBA.
+ - de descontos sem perder nadica, e faça o cliente ainda mais satisfeito.
